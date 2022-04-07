0308c4d2ca4ee6ac9e0dbc7cb7c8dfd8674fdf28
TheMrChaptastic <chappy@themrchaptastic.com>
2022-04-07 12:33:52 -0500

    Three Strong Reads added

---
+++ Live Interview Questions Tests/ThreeStrongReadsTests.cs
@@ -0,0 +1,21 @@
+﻿using Live_Interview_Questions;
+using NUnit.Framework;
+
+namespace Live_Interview_Questions_Tests
+{
+    public class ThreeStrongReadsTests
+    {
+        private QThreeStrongReads _threeStrongReads = new QThreeStrongReads();
+
+        [Test]
+        [TestCase(new[] { "a", "b", "c", "a", "a", "b", "c", "a", "b", "c", "b" }, new[] { 12, 9, 16, 11, 7, 12, 10, 9, 12, 11, 15 }, "b")]
+        [TestCase(new[] { "a", "b", "c", "a", "a", "b", "c", "a", "b", "c", "b" }, new[] { 12, 9, 16, 11, 16, 12, 10, 9, 12, 11, 15 }, "a")]
+        [TestCase(new[] { "d", "a", "b", "d", "c", "a", "a", "d", "b", "c", "a", "b", "c", "b" }, new[] { 11, 12, 9, 14, 16, 11, 7, 12, 12, 10, 9, 12, 11, 15 }, "d")]
+        public void ThreeStrongReadsTest(string[] ids, int[] strs, string exp)
+        {
+            var result = _threeStrongReads.FirstToThreeStrongReads(ids, strs);
+
+            Assert.AreEqual(exp, result);
+        }
+    }
+}
---
+++ Live Interview Questions/QThreeStrongReads.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Live_Interview_Questions
+{
+    /* Request: You will recieve two arrays of equal length. The first array will be string IDs and the second array will be the 
+     * signal strength associated with each read. So ids[0] will be the first scanned id and strength[0] will be that scans strength.
+     * A strong read(scan) will be a scan with a strength above 10 (x > 10) Your job is to return the id that is first to have 5 strong reads.
+     *
+     * Example: If ids = { "a", "b", "c", "a", "a", "b", "c", "a", "b", "c", "b" } and strength = { 12, 9, 16, 11, 7, 12, 10, 9, 12, 11, 15 }
+     * Then you would return "b" because its the the first to have 3 strengths over 10.
+     */
+    public class QThreeStrongReads
+    {
+        public string FirstToThreeStrongReads(string[] ids, int[] strength)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
