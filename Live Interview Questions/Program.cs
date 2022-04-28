6b90979eacc156370e0a7d8a951db99e798ad8ef
TheMrChaptastic <chappy@themrchaptastic.com>
2022-04-28 16:00:58 -0500

    added discord in comment

--- Live Interview Questions/Program.cs
+++ Live Interview Questions/Program.cs
@@ -6,7 +6,9 @@ namespace Live_Interview_Questions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");  //We Wont Be Doing Anything Here go to the Question Classes
+            Console.WriteLine("Hello World!");  //We Wont Be Doing Anything Here go to the Question Classes marked with Q(QuestionName).cs
+
+            //If you need any clarification or assistance hmu on discord @Chappy#0935
         }
     }
 }
