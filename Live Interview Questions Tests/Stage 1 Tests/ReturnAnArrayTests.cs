aff35635c2476532122ae3cf24fd926b498418b7
TheMrChaptastic <chappy@themrchaptastic.com>
2022-05-22 22:01:53 -0500

    Added question from internship level interview

---
+++ Live Interview Questions Tests/Stage 1 Tests/ReturnAnArrayTests.cs
@@ -0,0 +1,39 @@
+﻿using Live_Interview_Questions;
+using NUnit.Framework;
+
+namespace Live_Interview_Questions_Tests
+{
+    public class ReturnAnArrayTests
+    {
+        private QReturnAnArray _returnAnArray = new QReturnAnArray();
+
+        [Test]
+        public void ShouldBeEmptyStringArray()
+        {
+            var result = _returnAnArray.ReturnEmptyStringArray();
+
+            Assert.AreEqual(new string[0], result);
+        }
+
+        [Test]
+        public void ShouldBeEmptyIntArray()
+        {
+            var result = _returnAnArray.ReturnEmptyIntArray();
+
+            Assert.AreEqual(new int[0], result);
+        }
+
+        [Test]
+        [TestCase(1, new int[] { 1 })]
+        [TestCase(3, new int[] { 1, 2, 3 })]
+        [TestCase(5, new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(10, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })]
+        [TestCase(20, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 })]
+        public void ShouldBePopulatedFrom1ToN(int n, int[] expected)
+        {
+            var result = _returnAnArray.ReturnIntArrayFrom1ToN(n);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Live Interview Questions/Stage 1/QReturnAnArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Live_Interview_Questions
+{
+    public class QReturnAnArray
+    {
+        // RETURN AN EMPTY ARRAY OF STRINGS
+        public string[] ReturnEmptyStringArray()
+        {
+            throw new NotImplementedException();
+        }
+
+        // RETURN AN EMPTY ARRAY OF INTEGERS 
+        public int[] ReturnEmptyIntArray()
+        {
+            throw new NotImplementedException();
+        }
+
+        // RETURN AN ARRAY OF INTEGERS FROM 1 TO N
+        // Example: n = 1 would return int[] { 1 }.
+        // n = 3 would return int[] { 1, 2, 3 }.
+        public int[] ReturnIntArrayFrom1ToN(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
