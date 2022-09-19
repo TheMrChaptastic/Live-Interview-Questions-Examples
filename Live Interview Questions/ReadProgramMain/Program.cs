9bf2241baf10081896c820b46339a3d10b5de6ab
TheMrChaptastic <chappy@themrchaptastic.com>
2022-09-19 18:04:08 -0500

    Clarification in Directions

--- Live Interview Questions/ReadProgramMain/Program.cs
+++ Live Interview Questions/ReadProgramMain/Program.cs
@@ -6,18 +6,16 @@ namespace Live_Interview_Questions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");  // We Wont Be Doing Anything Here go to the Question Classes marked with Q(QuestionName).cs
+            Console.WriteLine("Hello World!"); // We Wont Be Doing Anything Here go to the Question Classes marked with Q(QuestionName).cs
             // Work in the Classes under the Live Interview Questions Project and write your code within those classes methods.
 
-            // This contains Questions collected from real Jr .NET Developer/.NET Developer/Software Engineer interviews.
-            // Most were layed out in a 1 hour session seperate from the Personal/Code Knowledge interview.
-            // They generally were 3-5 Questions starting with Stage 1 questions then moving into some Stage 2 questions and a few interviews moved into some Stage 3 questions. 
+            // This is Practice for Possible Live Coding Sections during Interviews! Practice on your own or with someone to give you the prompts and be able to explain your ideas for a solution before coding!
+            // Practice with 3-5 Questions starting with Stage 1 questions then moving into some Stage 2 questions and a few interviews moved into some Stage 3 questions. 
             // Try not to look up these questions online to better test your knowledge and get used to the interview scenario of anything could be shown to you. 
             // When solving these questions communication is important. Try to Say Aloud your idea for a solution, then code your idea. 
             // For this practice looking up correct way to write syntax is fine since you will be able to ask your interviewers to clarify Syntax you might not remember exact wording for. 
 
-            // If you need any clarification or assistance hmu on discord Chappy#0935 
-            // If you have a live interview and want to share your questions that we dont have listed please reach out to me!
+            // If you need any clarification, assistance or want to do a mock interview hmu on discord Chappy#0935
             // Go get that bread!
         }
     }
