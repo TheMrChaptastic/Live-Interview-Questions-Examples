dd24e541da565abb34386c7ff1a13b3ab00be9ce
TheMrChaptastic <chappy@themrchaptastic.com>
2022-10-08 16:54:18 -0500

    example with new test added

---
+++ Live Interview Questions Tests/Stage 1 Tests/ReturnAllEvenNumbersTests.cs
@@ -0,0 +1,40 @@
+﻿using Live_Interview_Questions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Live_Interview_Questions_Tests
+{
+    public class ReturnAllEvenNumbersTests
+    {
+        private QReturnAllEvenNumbers _returnAnArray = new QReturnAllEvenNumbers();
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(0)]
+        [TestCase(-100)]
+        [TestCase(null)]
+        public void ShouldBeEmptyIntList(int n)
+        {
+            var result = _returnAnArray.ReturnListEvensFrom1ToN(n);
+
+            Assert.AreEqual(new List<int>(), result);
+        }
+
+        [Test]
+        [TestCase(2, new int[] { 2 })]
+        [TestCase(6, new int[] { 2, 4, 6 })]
+        [TestCase(10, new int[] { 2, 4, 6, 8, 10 })]
+        [TestCase(20, new int[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 })]
+        [TestCase(50, new int[] { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50 })]
+        public void ShouldBePopulatedFrom1ToN(int n, int[] expected)
+        {
+            var result = _returnAnArray.ReturnListEvensFrom1ToN(n);
+
+            Assert.AreEqual(expected.ToList(), result);
+        }
+    }
+}
---
+++ Live Interview Questions/Stage 1/QReturnAllEvenNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Live_Interview_Questions
+{
+    public class QReturnAllEvenNumbers
+    {
+        /* Request: Return a List of type 'int' containing numbers from 1 to 'n'(parameter) that are EVEN numbers.
+        * 
+        * Example: n = 2 would return a list containing { 2 }. n = 5 would return a list containing { 2, 4 }.
+        * n = 10 would return a list containing { 2, 4, 6, 8, 10 }
+        */
+        public List<int> ReturnListEvensFrom1ToN(int n)
+        {
+            //var list = new List<int>();
+            //if (n == null || n <= 1)
+            //{
+            //    return list;
+            //}
+            //for(int i = 1; i <= n; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        list.Add(i);
+            //    }
+            //}
+            //return list;
+        }
+    }
+}
