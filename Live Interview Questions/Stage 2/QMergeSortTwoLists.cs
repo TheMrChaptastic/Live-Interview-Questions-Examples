f64f4f11b158a72fa5d85a1fb24ef73b7c9026cd
TheMrChaptastic <chappy@themrchaptastic.com>
2022-10-13 12:21:28 -0500

    added description for new question

--- Live Interview Questions/Stage 2/QMergeSortTwoLists.cs
+++ Live Interview Questions/Stage 2/QMergeSortTwoLists.cs
@@ -8,6 +8,13 @@ namespace Live_Interview_Questions
 {
     public class QMergeSortTwoLists
     {
+        /* Request: Return the a list that contains the sorted elements of both lists coming from the method parameters.
+        * 
+        * Example: { 1, 3, 2 } and { 5, 2 } would return { 1, 2, 2, 3, 5 }.
+        * { 5, 3, 2 } and { } would return { 2, 3, 5 }.
+        *
+        * Hint: Lists recieved could be empty but you still need to return a List of type int. Do not return null.
+        */
         public List<int> MergeSortTwoLists(List<int> list1, List<int> list2)
         {
             throw new NotImplementedException();
