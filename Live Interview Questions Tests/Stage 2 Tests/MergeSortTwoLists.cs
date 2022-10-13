03bcdec9fc653f2ec4a4e25afbef66dd3d11e49b
TheMrChaptastic <chappy@themrchaptastic.com>
2022-10-13 11:23:59 -0500

    new question and tests added

---
+++ Live Interview Questions Tests/Stage 2 Tests/MergeSortTwoLists.cs
@@ -0,0 +1,36 @@
+﻿using Live_Interview_Questions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Live_Interview_Questions_Tests
+{
+    public class MergeSortTwoLists
+    {
+        private QMergeSortTwoLists _sumTwoToTargetNumber = new QMergeSortTwoLists();
+
+        [Test]
+        public void ShouldReturnEmptyList()
+        {
+            var result = _sumTwoToTargetNumber.MergeSortTwoLists(new List<int>(), new List<int>());
+
+            Assert.AreEqual(new List<int>(), result);
+        }
+
+        [Test]
+        [TestCase(new[] { 1, 2, 3 }, new[] { 1, 2, 3 }, new[] { 1, 1, 2, 2, 3, 3 })]
+        [TestCase(new[] { 3, 2, 1 }, new[] { 1, 2, 3 }, new[] { 1, 1, 2, 2, 3, 3 })]
+        [TestCase(new[] { 1, 5, 9 }, new[] { 1, 2, 3 }, new[] { 1, 1, 2, 3, 5, 9 })]
+        [TestCase(new[] { 21, 1232, 33 }, new[] { 31, 22, 563 }, new[] { 21, 22, 31, 33, 563, 1232 })]
+        [TestCase(new[] { 1 }, new[] { 1, 4, 2, 3 }, new[] { 1, 1, 2, 3, 4 })]
+        public void ShouldSortMergedLists(int[] arr1, int[] arr2, int[] expected)
+        {
+            var result = _sumTwoToTargetNumber.MergeSortTwoLists(arr1.ToList(), arr2.ToList());
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Live Interview Questions/Stage 2/QMergeSortTwoLists.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Live_Interview_Questions
+{
+    public class QMergeSortTwoLists
+    {
+        public List<int> MergeSortTwoLists(List<int> list1, List<int> list2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
