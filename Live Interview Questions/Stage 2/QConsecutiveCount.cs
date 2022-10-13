4852e78220e7bb145fdae2e43e4e6059e4f4a5e0
TheMrChaptastic <chappy@themrchaptastic.com>
2022-10-13 10:44:44 -0500

    updated description to better clarify task

--- Live Interview Questions/Stage 2/QConsecutiveCount.cs
+++ Live Interview Questions/Stage 2/QConsecutiveCount.cs
@@ -9,8 +9,9 @@ namespace Live_Interview_Questions
     /* Request: Return the highest number of consecutive values in the array. 
     * 
     * Example: new int[] { 1, 2, 3, 4, 5, 6, 7, 8 } would return 1 since there are no back to backs of the same value.
-    * new int[] { 1, 3, 3, 3, 3, 5, 6} would return 4 since there are 4 3's next to each other.
+    * new int[] { 1, 3, 3, 3, 3, 5, 6} would return 4 since there are 4 3's next to each other. { 1, ((3, 3, 3, 3,)) 5, 6}
     * new int[] { 1, 2, 1, 2, 3, 1} would return 1 since no back to backs of the same value. 
+    * * new int[] { 1, 2, 1, 2, 2, 1} would return 2. { 1, 2, 1, ((2, 2,)) 1}
     * null int array will return 0;
     */
     public class QConsecutiveCount
